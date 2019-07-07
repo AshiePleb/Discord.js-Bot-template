166ddfec94d884ceafb81a5a0f5531bc29ab2680
AshleyHunter01 <rickettar03@gmail.com>
2019-07-07 19:48:53 +0100

    uploaded the bot files

---
+++ bot.js
@@ -0,0 +1,44 @@
+const Discord = require("discord.js");
+const fs = require("fs");
+const bot = new Discord.Client({disableEveryone: true});
+bot.commands = new Discord.Collection();
+const config = require("./config.json");
+
+bot.login(config.bot.token).then(function() {
+  console.log(`Connected to discord and logged in as ${bot.user.tag}`)
+}).catch((e) => {
+  console.log(e)
+})
+
+bot.on('ready', async() => {
+    bot.user.setActivity(`Oh I'm Alive!`)
+});
+
+fs.readdir('./commands', (err, files) => {
+    if (err) console.log(err)
+    let jsfile = files.filter(f => f.split(".").pop() === "js")
+    if (jsfile.length <= 0) {
+        console.log('No commands found!')
+        return;
+    }
+
+    jsfile.forEach((file, i) => {
+        let props = require(`./commands/${file}`)
+        console.log(`${file} has been loaded`)
+        bot.commands.set(props.help.name, props)
+    })
+
+    bot.on('message', async message => {
+        let prefix = config.bot.prefix
+
+        if (!message.content.startsWith(prefix)) return
+        if (message.author.bot || message.channel.type === "dm") return
+
+        let messageArray = message.content.split(' ')
+        let cmd = messageArray[0]
+        let args = messageArray.splice(1)
+        let commandFile = bot.commands.get(cmd.slice(prefix.length))
+
+        if (commandFile) commandFile.run(bot, message, args)
+    })
+})
---
+++ commands/ping.js
@@ -0,0 +1,20 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+    const useruser = "Requested By: " + message.author.tag;
+    const userurl = message.author.avatarURL;
+    let botembed = new Discord.RichEmbed()
+        .setColor("RANDOM")
+        .setDescription(`Getting Ping.`)
+    message.channel.send(botembed).then(message =>{
+        botembed.setColor("RANDOM")
+        botembed.setDescription(`:ping_pong: My Ping Is: **\`${bot.pings[0]}ms\`**`)
+        botembed.setFooter(useruser, userurl)
+        message.edit(botembed)
+    })
+
+}
+
+module.exports.help = {
+	name:"ping"
+}
